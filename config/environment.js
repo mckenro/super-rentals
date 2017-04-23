7889e8996bad966286595b03425d51a90f0aff4e
Rob McKenzie <mckenro@gmail.com>
2017-04-23 15:21:07 -0700

    add ability to add rentals

--- config/environment.js
+++ config/environment.js
@@ -11,12 +11,20 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
+
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
     },
 
+    firebase: {
+      apiKey: 'AIzaSyCDD9hFOqCKaNjsbGrNgl_C4ll2tDP8b1c',
+      authDomain: 'super-rentals-64f8f.firebaseapp.com',
+      databaseURL: 'https://super-rentals-64f8f.firebaseio.com',
+      storageBucket: 'super-rentals-64f8f.appspot.com'
+    },
+    
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created

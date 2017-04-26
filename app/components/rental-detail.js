d523d9c283513e872a9cbd3c455d5620708bc5b4
Rob McKenzie <mckenro@gmail.com>
2017-04-26 16:42:03 -0700

    add navbar and ability to delete rentals and reviews

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
       if (confirm('Do you really want to delete this rental?')) {
         this.sendAction('destroyRental', rental);
       }
+    },
+    destroyReview(review) {
+      this.sendAction('destroyReview', review);
     }
   }
 });
